05950cbd7ed9280538d1e7417d3a0cc05cd94aa8
Mahendranv2005 <mahendranvarudharajan@gmail.com>
2025-03-19 20:12:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var body = document.querySelector("body");
+
+function dark() {
+    body.className = "";
+    body.classList.add("dark");
+}
+
+function light() {
+    body.classList.add("light");
+}
+
+
